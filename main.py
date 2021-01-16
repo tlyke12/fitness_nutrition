ac0368fea2cb0113dff12c8e1f4b815cb049b45b
tlyke12 <tlyke7@gmail.com>
2021-01-16 13:06:51 -0600

    Update to allow record retrieval for testing

--- main.py
+++ main.py
@@ -14,4 +14,8 @@ details = nutri_connect.calc_workout(workout=workout)
 # Add workout and nutritional detail to spreadsheet via API
 sheety_connection.add_row(details=details)
 
-# records = sheety_connection.get_all_records()
+# Testing by retrieving and printing records in spreadsheet
+records = sheety_connection.get_all_records()
+for record in records['workouts']:
+    for item in record.items():
+        print(item)
